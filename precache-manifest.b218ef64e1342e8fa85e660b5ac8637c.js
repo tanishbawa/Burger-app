94baa97b51a44be08aa2e937622493a6c520914b
tanishbawa <tanishbawa15@gmail.com>
2020-10-12 23:29:09 +0530

    Updates

---
+++ precache-manifest.b218ef64e1342e8fa85e660b5ac8637c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdbf2fad3a1ccea529d8eb0afb76c0b3",
+    "url": "/Burger-app/index.html"
+  },
+  {
+    "revision": "c025f9eb3f68cdfba479",
+    "url": "/Burger-app/static/css/main.1a27d3ef.chunk.css"
+  },
+  {
+    "revision": "07b8e5948696f475beb3",
+    "url": "/Burger-app/static/js/2.2e9ae809.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Burger-app/static/js/2.2e9ae809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c025f9eb3f68cdfba479",
+    "url": "/Burger-app/static/js/main.5b2ad1a8.chunk.js"
+  },
+  {
+    "revision": "33b2c8d5a1e15037e040",
+    "url": "/Burger-app/static/js/runtime-main.658433b9.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger-app/static/media/burger-logo.b8503d26.png"
+  }
+]);
--- src/axios-orders.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: "https://react-my-burger-63465.firebaseio.com/"
-});
-
-export default instance;
